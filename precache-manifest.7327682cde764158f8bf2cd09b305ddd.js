799b74aaad0142ddb69557a35d47257aa9977351
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-05-30 13:03:27 -0400

    Updates

---
+++ precache-manifest.7327682cde764158f8bf2cd09b305ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b3393080add023073311e7d6da5ad4",
+    "url": "/React-Exercise/index.html"
+  },
+  {
+    "revision": "1a99fbd843e85223192f",
+    "url": "/React-Exercise/static/css/main.e5024099.chunk.css"
+  },
+  {
+    "revision": "d32e96662871e246f65a",
+    "url": "/React-Exercise/static/js/2.babb8949.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Exercise/static/js/2.babb8949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a99fbd843e85223192f",
+    "url": "/React-Exercise/static/js/main.a23a13df.chunk.js"
+  },
+  {
+    "revision": "2b45fa5c85fe0f407843",
+    "url": "/React-Exercise/static/js/runtime-main.fbf464a0.js"
+  }
+]);
